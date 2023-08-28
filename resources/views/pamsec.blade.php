0ce83124ae18f9c09e737a69c05aec89826ecf74
japheth jonathan <108800190+spicy3377@users.noreply.github.com>
2023-08-28 10:41:06 +0100

    cpamsec

--- resources/views/pamsec.blade.php
+++ resources/views/pamsec.blade.php
@@ -101,6 +101,8 @@
 									
 									
 								</div>
+
+								
 								<!--begin::Row-->
 								<div class="row g-5 g-xl-10">
 									<!--begin::Col-->
@@ -119,7 +121,7 @@
 																<span class="fs-4 fw-bold text-gray-400 align-self-start me-1&gt;">$</span>
 																<!--end::Currency-->
 																<!--begin::Value-->
-																<span class="fs-2hx fw-bolder text-gray-800 me-2 lh-1">69,700</span>
+																<span class="fs-2hx fw-bolder text-gray-800 me-2 lh-1">11</span>
 																<!--end::Value-->
 																<!--begin::Label-->
 																
@@ -127,7 +129,7 @@
 															</div>
 															<!--end::Info-->
 															<!--begin::Description-->
-															<span class="fs-6 fw-bold text-gray-400">Total Funds Staffs</span>
+															<span class="fs-6 fw-bold text-gray-400">Total No Of GM</span>
 															<!--end::Description-->
 														</div>
 														<!--end::Statistics-->
@@ -154,7 +156,8 @@
 															</div>
 															<!--end::Info-->
 															<!--begin::Subtitle-->
-															<span class="text-gray-400 pt-1 fw-bold fs-6">Total Revenue</span>
+															<span class="text-gray-400 pt-1 fw-bold fs-6">
+Total No of Funds Staffs</span>
 															<!--end::Subtitle-->
 														</div>
 														<!--end::Title-->
@@ -164,13 +167,8 @@
 													<div class="card-body d-flex align-items-end pt-0">
 														<!--begin::Progress-->
 														<div class="d-flex align-items-center flex-column mt-3 w-100">
-															<div class="d-flex justify-content-between w-100 mt-auto mb-2">
-																<span class="fw-boldest fs-6 text-dark">1,048 to Goal</span>
-																<span class="fw-bolder fs-6 text-gray-400">62%</span>
-															</div>
-															<div class="h-8px mx-3 w-100 bg-light-success rounded">
-																<div class="bg-success rounded h-8px" role="progressbar" style="width: 62%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-															</div>
+															
+															
 														</div>
 														<!--end::Progress-->
 													</div>
@@ -196,7 +194,8 @@
 															</div>
 															<!--end::Statistics-->
 															<!--begin::Description-->
-															<span class="fs-6 fw-bold text-gray-400">Total Fund Staffs</span>
+															<span class="fs-6 fw-bold text-gray-400">
+Total No of Certificate Issued</span>
 															<!--end::Description-->
 														</div>
 														<!--end::Statistics-->
@@ -214,10 +213,11 @@
 														<!--begin::Title-->
 														<div class="card-title d-flex flex-column">
 															<!--begin::Amount-->
-															<span class="fs-2hx fw-bolder text-dark me-2 lh-1 ls-n2">6.3k</span>
+															<span class="fs-2hx fw-bolder text-dark me-2 lh-1 ls-n2">354</span>
 															<!--end::Amount-->
 															<!--begin::Subtitle-->
-															<span class="text-gray-400 pt-1 fw-bold fs-6">Net Income</span>
+															<span class="text-gray-400 pt-1 fw-bold fs-6">
+Total No of Employers</span>
 															<!--end::Subtitle-->
 														</div>
 														<!--end::Title-->
@@ -229,6 +229,67 @@
 												<!--end::Card widget 7-->
 											</div>
 										</div>
+									</div>
+
+									<div class="col-xl-6">
+										<!--begin::Chart widget 15-->
+										<div class="card card-flush h-xl-100">
+											<!--begin::Header-->
+											<div class="card-header pt-7">
+												<!--begin::Title-->
+												<h3 class="card-title align-items-start flex-column">
+													<span class="card-label fw-bolder text-dark">Annual Expenditure </span>
+													<!-- <span class="text-gray-400 pt-2 fw-bold fs-6">per Year</span> -->
+												</h3>
+												<!--end::Title-->
+												<!--begin::Toolbar-->
+												<div class="card-toolbar">
+													<!--begin::Menu-->
+													<button class="btn btn-icon btn-color-gray-400 btn-active-color-primary justify-content-end" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end" data-kt-menu-overflow="true">
+														<!--begin::Svg Icon | path: icons/duotune/general/gen023.svg-->
+														<span class="svg-icon svg-icon-1 svg-icon-gray-300 me-n1">
+															<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+																<rect opacity="0.3" x="2" y="2" width="20" height="20" rx="4" fill="currentColor" />
+																<rect x="11" y="11" width="2.6" height="2.6" rx="1.3" fill="currentColor" />
+																<rect x="15" y="11" width="2.6" height="2.6" rx="1.3" fill="currentColor" />
+																<rect x="7" y="11" width="2.6" height="2.6" rx="1.3" fill="currentColor" />
+															</svg>
+														</span>
+														<!--end::Svg Icon-->
+													</button>
+													<!--begin::Menu-->
+													<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold w-100px py-4" data-kt-menu="true">
+														<!--begin::Menu item-->
+														<div class="menu-item px-3">
+															<a href="#" class="menu-link px-3">Remove</a>
+														</div>
+														<!--end::Menu item-->
+														<!--begin::Menu item-->
+														<div class="menu-item px-3">
+															<a href="#" class="menu-link px-3">Mute</a>
+														</div>
+														<!--end::Menu item-->
+														<!--begin::Menu item-->
+														<div class="menu-item px-3">
+															<a href="#" class="menu-link px-3">Settings</a>
+														</div>
+														<!--end::Menu item-->
+													</div>
+													<!--end::Menu-->
+													<!--end::Menu-->
+												</div>
+												<!--end::Toolbar-->
+											</div>
+											<!--end::Header-->
+											<!--begin::Body-->
+											<div class="card-body pt-5">
+												<!--begin::Chart container-->
+												<div id="kt_charts_widget_15_chart" class="min-h-auto ps-4 pe-6 mb-3 h-350px"></div>
+												<!--end::Chart container-->
+											</div>
+											<!--end::Body-->
+										</div>
+										<!--end::Chart widget 15-->
 									</div>
                                    
                                     
