960b6fa9a5695e3a8e8fad0438e5fc89d0e17192
Abdul5050 <tacticshustle@gmail.com>
2023-10-26 19:57:46 +0100

    Modified the ranking migration file and other stuff

--- Modules/HumanResource/Database/Migrations/2023_05_15_102217_create_rankings_table.php
+++ Modules/HumanResource/Database/Migrations/2023_05_15_102217_create_rankings_table.php
@@ -13,11 +13,11 @@ class CreateRankingsTable extends Migration
      */
     public function up()
     {
-        Schema::create('rankings', function (Blueprint $table) {
+        /* Schema::create('rankings', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
-        });
+        }); */
     }
     
 
