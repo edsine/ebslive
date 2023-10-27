db913804a4730e0377b20a159ff77d6f697ec832
AbelPaul <abelpaul12@gmail.com>
2023-10-27 13:12:15 +0100

    worked on chart

--- Modules/Accounting/Resources/views/dashboard/account-dashboard.blade.php
+++ Modules/Accounting/Resources/views/dashboard/account-dashboard.blade.php
@@ -657,5 +657,188 @@
             </div>
         </div>
     </div>
-    
+    <script>
+          @if(\Auth::user()->can('show account dashboard'))
+    (function () {
+        var chartBarOptions = {
+            series: [
+                {
+                    name: "{{__('Income')}}",
+                    data:{!! json_encode($incExpLineChartData['income']) !!}
+                },
+                {
+                    name: "{{__('Expense')}}",
+                    data: {!! json_encode($incExpLineChartData['expense']) !!}
+                }
+            ],
+
+            chart: {
+                height: 250,
+                type: 'area',
+                // type: 'line',
+                dropShadow: {
+                    enabled: true,
+                    color: '#000',
+                    top: 18,
+                    left: 7,
+                    blur: 10,
+                    opacity: 0.2
+                },
+                toolbar: {
+                    show: false
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                width: 2,
+                curve: 'smooth'
+            },
+            title: {
+                text: '',
+                align: 'left'
+            },
+            xaxis: {
+                categories:{!! json_encode($incExpLineChartData['day']) !!},
+                title: {
+                    text: '{{ __("Days") }}'
+                }
+            },
+            colors: ['#6fd944', '#6fd944'],
+
+            grid: {
+                strokeDashArray: 4,
+            },
+            legend: {
+                show: false,
+            },
+            // markers: {
+            //     size: 4,
+            //     colors: ['#ffa21d', '#FF3A6E'],
+            //     opacity: 0.9,
+            //     strokeWidth: 2,
+            //     hover: {
+            //         size: 7,
+            //     }
+            // },
+            yaxis: {
+                title: {
+                    text: '{{ __("Amount") }}'
+                },
+
+            }
+
+        };
+        var arChart = new ApexCharts(document.querySelector("#cash-flow"), chartBarOptions);
+        arChart.render();
+    })();
+    (function () {
+        var options = {
+            chart: {
+                height: 180,
+                type: 'bar',
+                toolbar: {
+                    show: false,
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                width: 2,
+                curve: 'smooth'
+            },
+            series: [{
+                name: "{{__('Income')}}",
+                data: {!! json_encode($incExpBarChartData['income']) !!}
+            }, {
+                name: "{{__('Expense')}}",
+                data: {!! json_encode($incExpBarChartData['expense']) !!}
+            }],
+            xaxis: {
+                categories: {!! json_encode($incExpBarChartData['month']) !!},
+            },
+            colors: ['#3ec9d6', '#FF3A6E'],
+            fill: {
+                type: 'solid',
+            },
+            grid: {
+                strokeDashArray: 4,
+            },
+            legend: {
+                show: true,
+                position: 'top',
+                horizontalAlign: 'right',
+            },
+            // markers: {
+            //     size: 4,
+            //     colors: ['#3ec9d6', '#FF3A6E',],
+            //     opacity: 0.9,
+            //     strokeWidth: 2,
+            //     hover: {
+            //         size: 7,
+            //     }
+            // }
+        };
+        var chart = new ApexCharts(document.querySelector("#incExpBarChart"), options);
+        chart.render();
+    })();
+
+    (function () {
+        var options = {
+            chart: {
+                height: 140,
+                type: 'donut',
+            },
+            dataLabels: {
+                enabled: false,
+            },
+            plotOptions: {
+                pie: {
+                    donut: {
+                        size: '70%',
+                    }
+                }
+            },
+            series: {!! json_encode($expenseCatAmount) !!},
+            colors: {!! json_encode($expenseCategoryColor) !!},
+            labels: {!! json_encode($expenseCategory) !!},
+            legend: {
+                show: true
+            }
+        };
+        var chart = new ApexCharts(document.querySelector("#expenseByCategory"), options);
+        chart.render();
+    })();
+
+    (function () {
+        var options = {
+            chart: {
+                height: 140,
+                type: 'donut',
+            },
+            dataLabels: {
+                enabled: false,
+            },
+            plotOptions: {
+                pie: {
+                    donut: {
+                        size: '70%',
+                    }
+                }
+            },
+            series: {!! json_encode($incomeCatAmount) !!},
+            colors: {!! json_encode($incomeCategoryColor) !!},
+            labels:  {!! json_encode($incomeCategory) !!},
+            legend: {
+                show: true
+            }
+        };
+        var chart = new ApexCharts(document.querySelector("#incomeByCategory"), options);
+        chart.render();
+    })();
+    @endif
+</script>
+    </script>
 @endsection
