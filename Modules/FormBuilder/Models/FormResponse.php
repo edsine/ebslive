1c9de47b697552bf14ad14a84cce622e65ebc96c
Abdul5050 <tacticshustle@gmail.com>
2023-12-06 08:14:03 +0100

    Form builder response message fixed

--- Modules/FormBuilder/Models/FormResponse.php
+++ Modules/FormBuilder/Models/FormResponse.php
@@ -3,9 +3,11 @@
 namespace Modules\FormBuilder\Models;
 
 use Illuminate\Database\Eloquent\Model;
+//use App\Traits\Approval;
 
 class FormResponse extends Model
 {
+   // use Approval;
     protected $fillable = [
         'form_id',
         'response',
